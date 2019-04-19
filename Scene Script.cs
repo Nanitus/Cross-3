65a061822f03e8fa2517792abba4db7d4828c429
Nanitus <44480655+Nanitus@users.noreply.github.com>
2019-04-18 20:29:43 -0400

    Add files via upload

---
+++ Scene Script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM_SceneLoader : MonoBehaviour
+{
+
+    public void START()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void RETRY()
+    {
+        SceneManager.LoadScene(1);
+        GameManager.instance.Health = 3;
+    }
+
+    public void CONTROLS()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void BACK()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void RELOAD()
+    {
+        SceneManager.LoadScene(0);
+        GameManager.instance.Health = 3;
+        GameManager.instance.Lives = 3;
+    }
+
+    public void EXIT()
+    {
+        Application.Quit();
+    }
+}
